a26489b1ca4ff7c97aa12b9656ac7b285e9c7b67
Minh Pham <minhpham@myhost.local>
2017-12-25 09:45:34 +0700

    2 - Change to redux project structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 class App extends Component {
+
 	render() {
+
+		const renderTodo = todo => {
+			<li key={todo.id}>
+				{todo.text} {todo.id}
+			</li>
+		}
+
 		return (
-			<div>App</div>
+			<div>
+				<h2>Todos</h2>
+				<input type="text" placeholder="Type a todo"/>
+				{ this.props.todos.map(renderTodo) }
+			</div>
 		);
 	}
 }
 
-export default App
+App.propTypes = {
+	todos: PropTypes.array.isRequired,
+}
+
+const mapStateToProps = state => ({
+	todos: state.todos,
+})
+
+export default connect(mapStateToProps, null)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import todos from './todos';
+
 
 export default combineReducers({
   state: (state = {}) => state,
+  todos,
   router: routerReducer
 });
